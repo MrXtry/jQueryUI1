9b7fa7bf78c75f43c7cac1e362ffd9b8520ede51
kevin larsson <larsson__96@hotmail.com>
2016-10-10 14:09:57 +0200

    done

---
+++ Scripts/Components/Droppable/droppable.component.js
@@ -0,0 +1,24 @@
+﻿/// <reference path="../../angular.js" />
+
+angular.module("mainModule")
+    .component("droppable", {
+        templateUrl: "Scripts/Components/Droppable/Droppable.html",
+        controller: function () {
+            var scope = this;
+            var component = {};
+
+            setTimeout(function () {
+                component = $("#droppable-" + scope.id);
+                component.droppable({
+                    drop: function (event, ui) {
+                        //console.log("Dropped in " + scope.id);
+                        console.log(ui.draggable);
+                    }
+                });
+            });
+        },
+        controllerAs: "scope",
+        bindings: {
+            id: "=",
+        }
+    });
